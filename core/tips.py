7bafdd4ed6158bbaecc5102c75299b31a5c84475
Yuri Astrov <yuriastrov@gmail.com>
2013-08-20 16:06:31 +0400

    Update tips.py

--- core/tips.py
+++ core/tips.py
@@ -3,9 +3,16 @@
 
 __doc__ = """Basic tips and trics.
 """
+a = 3
+b = 7
+#Swap
+a, b = b, a
 
 with open(filename, 'wb', encoding='utf-8') as f:
     f.write(3)
+
+with open(filename1, 'r') as fi, open(filename2, 'w') as fo:
+    pass
 
 from io import StringIO, BytesIO
 #StringIO Реализует интерфейс работы с файлами для строк
