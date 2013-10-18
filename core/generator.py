2281b3470b66d497da7954e3317b66b8eed6e19f
Yuri Astrov <yuriastrov@gmail.com>
2013-10-18 12:46:02 +0400

    Update generator.py

--- core/generator.py
+++ core/generator.py
@@ -4,11 +4,11 @@
 __doc__ = """Generator examples, snippets.
 """
 
-def numerate(lst, num=0):
+def numerate(iterable, num=0):
     """Core enumerate function analog"""
     n = 0
-    for l in lst:
-        yield (n, l)
+    for x in iterable:
+        yield (n, x)
         n += 1
 
 def filereader(fname, encoding='utf-8'):
@@ -23,11 +23,69 @@ def filereader(fname, encoding='utf-8'):
             yield l
 
 import os
-def walk(self, path):
+def walk(path):
     for root, dirs, files in os.walk(path):
         for name in files:
             fname = os.path.join(root, name)
             yield fname
+
+def fibonacchi(n):
+    """
+    Fibonacchi numbers.
+    >>> list(fibonacchi(5))
+    [0, 1, 1, 2, 3, 5]
+    """
+    a, b = 0, 1
+    yield a
+    yield b
+    i = 2
+    n += 1
+    # for _ in range(2, n):
+    while i < n:
+        F = b+a
+        yield F
+        a = b
+        b = F
+        i+=1
+
+def get_pair(iterable, fil=None):
+    """
+    get_pair([1,2,3,4]) --> [1,2] [3,4]
+    >>> list(get_pair([1, 2, 3, 4]))
+    [[1, 2], [3, 4]]
+    >>> list(get_pair([1, 2, 3]))
+    [[1, 2], [3, None]]
+    """
+    pool = list(iterable)
+    if len(pool)%2 != 0:
+        pool.append(fil)
+    i = 0
+    while i < len(pool):
+        yield pool[i:i+2]
+        i += 2
+
+def get_pair2(iterable, fil=None):
+    """
+    get_pair2([1,2,3,4]) --> [1,2] [3,4]
+    >>> list(get_pair2([1, 2, 3, 4]))
+    [[1, 2], [3, 4]]
+    >>> list(get_pair2([1, 2, 3]))
+    [[1, 2], [3, None]]
+    """
+    result = [None, None]
+    # If comment next block, you may use it with
+    # all iterators, but the last element without a pair will be skipped.
+    # pool = iterable
+    # start
+    pool = list(iterable)
+    if len(pool)%2 != 0:
+        pool.append(fil)
+    # end
+    it = iter(pool)
+    while True:
+        result[0] = next(it)
+        result[1] = next(it)
+        yield result
 
 if __name__ == '__main__':
     for n, el in numerate(['a','b','c']):
