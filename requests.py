869da9efd514d2870870b5e0a9b01571cd30f727
Yuri Astrov <yuriastrov@gmail.com>
2013-12-13 17:27:52 +0300

    Update requests.py

--- requests.py
+++ requests.py
@@ -28,6 +28,18 @@ def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+        f.flush()
+        os.fsync(f.fileno())
+    return local_filename
+
 def download_file(url,
                     local_fname=None,
                     file_path=None,
