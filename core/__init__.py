bdf9de72a8c28ac02b70cff33d0e5bcfa9f57aee
Yuri Astrov <yuriastrov@gmail.com>
2014-04-01 17:14:25 +0400

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Use this for crossplatform package with next struct:
+    package
+        __init__.py (This file)
+        a.py
+        linux2
+            b.py
+        win32
+            b.py
+
+Ant then just use:
+from package import b
+
+
+Thank for http://asvetlov.blogspot.ru/2010/05/blog-post.html
+
+And some about import from zip:
+http://asvetlov.blogspot.ru/2010/05/3.html
+"""
+
+import sys
+from os.path import join, dirname
+
+__path__.append(join(dirname(__file__), sys.platform))
