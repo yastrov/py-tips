0bc72b51ae8e023d800344c51bb40955ed463d88
Yuri Astrov <yuriastrov@gmail.com>
2014-07-12 14:59:41 +0400

    New tips.

--- core/tips.py
+++ core/tips.py
@@ -12,7 +12,7 @@ import os
 pjoin = os.path.join
 
 # Advanced unpack
-a, b, *args, c  = list(range(10))
+a, b, *args, c  = range(10)
 
 # Unix epoha time in secunds
 epoch_time = int(time.time())
@@ -33,13 +33,23 @@ with open(filename, 'wb', encoding='utf-8') as f:
 with open(filename1, 'r') as fi, open(filename2, 'w') as fo:
     pass
 
+# Read chunks with size 32, while data from file is not '' 
+from functools import partial
+with open() as f
+    chunks = [chunk for chunk in iter(partial(f.read, 32), '')]
+
 from io import StringIO, BytesIO
 #StringIO Реализует интерфейс работы с файлами для строк
 #BytesIO Реализует интерфейс работы с файлами для строк
 #В Python работа идет в соответствии с интерфейсом, а не типом объекта.
 
-def foo(arg=None):
+# for access to vars:
+vars() # Also vars(obj) if obj has __dict__
+locals()
+
+def foo(arg=None, key=None):
     x = arg or []
+    key = key or lambda x: x
 
 def func(*args, **kwargs):
     print(args)
@@ -47,7 +57,8 @@ def func(*args, **kwargs):
 
 func(1, [2,3,4], age=23) # age in kwargs, others in args
 func({"age":23}) # dict in args
-func(**{"age":23}) # dict in kwargs
+func(**{"age":23}) # dict in kwargs, '**' - unpack dict
+func(*[1,2,3]) # Unpack list to *args
 
 #Keyword only argumant
 def f(a, b, *args, option=True):
@@ -60,7 +71,7 @@ def best_return():
     Оптимальный код возврата из функции - без предварительного создания переменной.
     """
     # Not a = (1,2)
-    return (1, 2)
+    return 1, 2
 
 import sys
 if sys.stdout.isatty():
