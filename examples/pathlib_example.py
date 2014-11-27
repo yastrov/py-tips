ec70f68c0e007351accaf668610182ff5c86cb0d
Yuri Astrov <yuriastrov@gmail.com>
2014-11-27 21:53:34 +0300

    Pathlib sample added.

--- examples/pathlib_example.py
+++ examples/pathlib_example.py
@@ -8,3 +8,21 @@ for path in Path('.').rglob('*.*'):
     print('File name:', path.name)
     print('Suffix:', path.suffix)
     print('Absolute path as str:', path.resolve())
+
+# Copy to previous for previous folder
+from shutil import move, copy
+root_dir = ''
+#dest_dir = ''
+src = Path(root_dir)
+#dest = Path(dest_dir)
+for file in src.rglob('*.jpg'):
+    dest = file.absolute().parent.parent
+    #dest_file = dest / file.relative_to(src)
+    dest_file = dest / file.name
+    try:
+        if not dest_file.parent.exists():
+            dest_file.parent.mkdir(parents=True)
+    except OSError as e:
+        print(e)
+    #move(str(file), str(dest_file))
+    copy(str(file), str(dest_file))
